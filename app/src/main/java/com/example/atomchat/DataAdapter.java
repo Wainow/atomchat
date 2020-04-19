79f5149f413a4474bac83d7088dced38aa85c8b6
Wainow <skylla00@mail.ru>
2020-04-19 22:39:00 +0400

    Absolutely circle beauty messages and incognito button

--- app/src/main/java/com/example/atomchat/DataAdapter.java
+++ app/src/main/java/com/example/atomchat/DataAdapter.java
@@ -8,14 +8,22 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.ArrayList;
 
 public class DataAdapter extends RecyclerView.Adapter<ViewHolder> {
 
-    ArrayList<String> messages;
+    private FirebaseUser user;
+
+    private static final int MSG_TYPE_LEFT = 0;
+    private static final int MSG_TYPE_RIGHT = 1;
+
+    ArrayList<Chat> messages;
     LayoutInflater inflater;
 
-    public DataAdapter(Context context, ArrayList<String> messages) {
+    public DataAdapter(Context context, ArrayList<Chat> messages) {
         this.messages = messages;
         this.inflater = LayoutInflater.from(context);
     }
@@ -23,18 +31,34 @@ public class DataAdapter extends RecyclerView.Adapter<ViewHolder> {
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.item_message, parent, false);
-        return new ViewHolder(view);
+
+        if(viewType == MSG_TYPE_RIGHT){
+            View view = inflater.inflate(R.layout.item_message_real, parent, false);
+            return new ViewHolder(view);
+        } else {
+            View view = inflater.inflate(R.layout.item_message, parent, false);
+            return new ViewHolder(view);
+        }
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        String msg = messages.get(position);
-        holder.message.setText(msg);
+        Chat chat = messages.get(position);
+        holder.message.setText(chat.getMessage());
     }
 
     @Override
     public int getItemCount() {
         return messages.size();
+    }
+
+    @Override
+    public int getItemViewType(int position){
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if(messages.get(position).getSender().equals(user.getUid())){
+            return MSG_TYPE_RIGHT;
+        } else {
+            return  MSG_TYPE_LEFT;
+        }
     }
 }
